f4bcf86c4be669720c61385ff1cdc19ef6f71a35
Jessica <jessica.galvan@hotmail.com>
2020-05-31 15:36:49 -0300

    Cosas varias + Door & Key
    El ataque se paso a trigger para que no empuje al jugador, pero ahora no rebota. Falta arreglar.
    Se empezo a hacer la logica del Line Renderer y el Raycast, pero por ahora no se visualizan. (aunque supuestamente, segun el debug, funcionan) 
    Se agrego un cuadrado verde que representa la puerta y un circulo amarillo que representa la llave. Y su logica correspondiente. Pero falta la pantalla de victoria o lo que tenga que pasar cuando llega a la puerta y tiene la llave. 
    Además de cositas que ya no recuerdo.

--- Assets/Script/Player/PlayerBehaviour.cs
+++ Assets/Script/Player/PlayerBehaviour.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 using Vector2 = UnityEngine.Vector3;
 using System.Threading;
+using UnityEditor.Experimental.Rendering;
 //using Quaternion = UnityEngine.Quaternion;
 
 public class PlayerBehaviour : MonoBehaviour
@@ -26,20 +27,22 @@ public class PlayerBehaviour : MonoBehaviour
 
 	//Raycast
 	private Vector3 actualPositionMouse;
-	private LineRenderer myLineRenderer;
+	private LineRenderer laser;
 	private RaycastHit2D hit2D;
 	[SerializeField]
 	private LayerMask layersToHit;
 	[SerializeField]
 	private float rayLenght = 5f;
-	private int currentImpact;
 	[SerializeField]
-	private int maxImpact;
+	private int reflections;
 
+	//Condicion de ganar
+	private bool llave;
 
 	private void Start()
 	{
-		myLineRenderer = GetComponent<LineRenderer>();
+		laser = GetComponent<LineRenderer>();
+		llave = false;
 	}
 
 
@@ -51,7 +54,7 @@ public class PlayerBehaviour : MonoBehaviour
 		movement.y = Input.GetAxisRaw("Vertical");
 		movementDirection = new Vector2(movement.x, movement.y);
 
-		//Animator.
+		//Animator. Si el jugador deja de moverse, se queda mirando en el sentido en que estaba yendo
 		if (movementDirection != Vector2.zero)
 		{
 			animator.SetFloat("Horizontal", movement.x);
@@ -62,32 +65,52 @@ public class PlayerBehaviour : MonoBehaviour
 		//MOUSE POSITION
 		actualPositionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-		//Hacer que salga un proyectil al apretar el boton izquierdo del mouse.
+		//Hacer que salga un proyectil al apretar el boton izquierdo del mouse, e inicia la animacion del ataque
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
-			GameObject shootObject = Instantiate(proyectil, firePoint.transform.position, transform.rotation);
+			Instantiate(proyectil, firePoint.transform.position, transform.rotation);
 			animator.SetFloat("OnAttack", 1f);
 		}
+		//Cuando el jugador deja de apretar el boton, se termina la animación. HAY QUE CAMBIARLO por un timer esto. 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
 			animator.SetFloat("OnAttack", 0);
 		}
 
 		//RAYCAST
-		/*hit2D = Physics2D.Raycast(transform.position, actualPositionMouse, rayLenght);
+		hit2D = Physics2D.Raycast(transform.position, actualPositionMouse, rayLenght);
+		float remainingLenght = rayLenght;
+		var ray = new Ray(transform.position, transform.right); //ver que onda acá
+		
 		if (hit2D)
 		{
-			Debug.DrawLine(transform.position, actualPositionMouse, Color.red);
-			if(hit2D.)
-			Debug.Log("hola");
-		}*/
+			laser.positionCount = 1;
+			laser.SetPosition(0, transform.position);
+			//laser.sortingOrder = 4;
+			//laser.sortingLayerName = "UI";
+
+			for (int i = 0; i < reflections; i++) 
+			{
+				
+				laser.positionCount += 1;
+				laser.SetPosition(laser.positionCount - 1, hit2D.point);
+				remainingLenght -= Vector2.Distance(ray.origin, hit2D.point);
+				ray = new Ray(hit2D.point, Vector2.Reflect(ray.direction, hit2D.normal));
+				//Debug.Log("Hola " + i);
+                //if (laser){ Debug.Log("Chau" + i + laser.positionCount); }
+			}
+		} /*else
+        {
+			laser.positionCount += 1;
+			laser.SetPosition(laser.positionCount - 1, ray.origin + ray.direction * remainingLenght);
+        }*/
 	}
 
 	private void FixedUpdate()
 	{
 		rb.MovePosition((Vector2)rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
 
-		//Para obtener la direccion a cual mirar segun donde este el mouse. 
+		//Para obtener la direccion a cual mirar segun donde este el mouse. NO BORRAR
 		/*Vector2 lookDir = (Vector2)actualPositionMouse - (Vector2)rb.position;
 		float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 		rb.rotation = angle;*/
@@ -103,6 +126,36 @@ public class PlayerBehaviour : MonoBehaviour
 	}
 	void PlayerDie()
 	{
+		//Comente esto porque me pudri de perder cuando necesito probar cosas.
 		//Destroy(gameObject);
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+		//si la colision tiene tag Key
+        if (col.gameObject.CompareTag("Key")) 
+		{
+			Debug.Log("Obtuviste una llave");
+			//Cambiame el bool a true
+			llave = true;
+			//y borrame el objeto llave de la escena
+			var objeto = col.gameObject;
+			Destroy(objeto);
+		}
+
+		//si la colision tiene tag Door
+		if (col.gameObject.CompareTag("Door"))
+		{
+			//y tiene el bool llave en true
+            if (llave)
+            {
+				//Acá iria a la pantalla de victoria o que aparezca visualemente en el canvas
+				Debug.Log("Ganaste");
+            } else
+            {
+				//Acá pasaria algo quizas? Un cartel que diga que falta la llave, o bueno, nada.
+				Debug.Log("No tenes la llave");
+            }
+		}
 	}
 }
