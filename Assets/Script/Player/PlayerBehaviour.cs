2fa8b6a61f416ce20f0b74dd4dd4001ad0be607c
Jessica <jessica.galvan@hotmail.com>
2020-06-01 00:44:06 -0300

    Walk Animation Fix + Collectables + Player Attack Cooldown
    Además complete las animaciones de los enemigos (pero no el animator controller o el codigo!) y un mini timer para el brazo ya que sino se bajaba muy rapido y quedaba raro.

--- Assets/Script/Player/PlayerBehaviour.cs
+++ Assets/Script/Player/PlayerBehaviour.cs
@@ -17,12 +17,16 @@ public class PlayerBehaviour : MonoBehaviour
 	[SerializeField]
 	private float moveSpeed = 5f;
 	[SerializeField]
-	private float timeLeftAttack = 2f;
+	private float cooldownAttack = 5f;
 	//Raycast
 	[SerializeField]
 	private float rayLenght = 5f;
 	[SerializeField]
 	private int reflections;
+	[SerializeField]
+	private bool canDie;
+	[SerializeField]
+	private int coins = 0;
 
 	[Header("Objetos")]
 	[SerializeField]
@@ -45,23 +49,32 @@ public class PlayerBehaviour : MonoBehaviour
 	//[SerializeField]
 	//private LayerMask layersToHit;
 
-
 	//Otros
 	private Vector2 movement;
 	private Vector2 movementDirection;
 	private bool llave;
+	//Timer cooldown
+	private bool canMove = true;
+	private bool canAttack = true;
+	private bool canCount = false;
+	private float timer;
+	//Timer Animation
+	private bool canCount2 = false;
+	private bool canAnimateAttack = false;
+	private float timerAnimation;
+	private float extendAnimation = 1f;
 
 	private void Start()
 	{
 		laser = GetComponent<LineRenderer>();
 		llave = false;
+		timer = cooldownAttack;
+		canDie = true;
 	}
-
 
 	void Update()
 	{
-
-		//Capta movimientos Horizontales y Verticales. 
+        //Capta movimientos Horizontales y Verticales. 
 		movement.x = Input.GetAxisRaw("Horizontal");
 		movement.y = Input.GetAxisRaw("Vertical");
 		movementDirection = new Vector2(movement.x, movement.y);
@@ -78,60 +91,91 @@ public class PlayerBehaviour : MonoBehaviour
 		actualPositionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 		//Hacer que salga un proyectil al apretar el boton izquierdo del mouse, e inicia la animacion del ataque
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetButtonDown("Fire1") && canAttack)
 		{
-			//Instantiate(proyectil, firePoint.transform.position, transform.rotation);
+			//canMove es para que no se pueda mover mientras este apretando el mouse.
+			canMove = false;
 			animator.SetFloat("OnAttack", 1f);
-			
-			//TIMER para que se cambie la animacion del ataque //NO FUNCIONA
-			timeLeftAttack -= Time.deltaTime;
-			if (timeLeftAttack <= 0f)
-			{
-				Debug.Log("Chau");
-				animator.SetFloat("OnAttack", 0f);
-				timeLeftAttack = 2f;
-			}
+			//acá tendria que hacer el raycast y line renderer para ver como va a ir el tiro que quiere hacer.  y que solo aparezca mientras se matiene apretado/down. Cuando va a up desaparece
 		}
 		
-		//Cuando el jugador deja de apretar el boton, se termina la animación. HAY QUE CAMBIARLO por un timer esto. 
-		if (Input.GetButtonUp("Fire1"))
+		//Cuando el jugador deja de apretar el boton, se termina la animación. 
+		if (Input.GetButtonUp("Fire1") && canAttack)
 		{
 			Instantiate(proyectil, firePoint.transform.position, transform.rotation);
-			animator.SetFloat("OnAttack", 0);
+			canMove = true;
+			//Start cooldown attack
+			canCount = true;
+			canAttack = false;
+			timer = cooldownAttack;
+			//un segundo timer que extiende un segundo la animación del brazo, me parece que queda mejor. 
+			timerAnimation = 0.1f;
+			canAnimateAttack = true;
+			canCount2 = true;
 		}
 
 		//RAYCAST
 		hit2D = Physics2D.Raycast(transform.position, actualPositionMouse, rayLenght);
-			float remainingLenght = rayLenght;
-			var ray = new Ray(transform.position, transform.right);
-		    Debug.DrawRay(transform.position, transform.right *100, Color.red);//ver que onda acá
+		float remainingLenght = rayLenght;
+		var ray = new Ray(transform.position, transform.right);
+		Debug.DrawRay(transform.position, transform.right *100, Color.red);//ver que onda acá
 
-			if (hit2D)
+		if (hit2D)
+		{
+			laser.positionCount = 1;
+			laser.SetPosition(0, transform.position);
+			//laser.sortingOrder = 4;
+			//laser.sortingLayerName = "UI";
+
+			for (int i = 0; i < reflections; i++)
 			{
-				laser.positionCount = 1;
-				laser.SetPosition(0, transform.position);
-				//laser.sortingOrder = 4;
-				//laser.sortingLayerName = "UI";
 
-				for (int i = 0; i < reflections; i++)
-				{
-
-					laser.positionCount += 1;
-					laser.SetPosition(laser.positionCount - 1, hit2D.point);
-					remainingLenght -= Vector2.Distance(ray.origin, hit2D.point);
-					ray = new Ray(hit2D.point, Vector2.Reflect(ray.direction, hit2D.normal));
-					//Debug.Log("Hola " + i);
-					//if (laser){ Debug.Log("Chau" + i + laser.positionCount); }
-				}
-			} /*else
-			{
 				laser.positionCount += 1;
-				laser.SetPosition(laser.positionCount - 1, ray.origin + ray.direction * remainingLenght);
-			}*/
+				laser.SetPosition(laser.positionCount - 1, hit2D.point);
+				remainingLenght -= Vector2.Distance(ray.origin, hit2D.point);
+				ray = new Ray(hit2D.point, Vector2.Reflect(ray.direction, hit2D.normal));
+				//Debug.Log("Hola " + i);
+				//if (laser){ Debug.Log("Chau" + i + laser.positionCount); }
+			}
+		} 
+		/*else
+		{
+		laser.positionCount += 1;
+		laser.SetPosition(laser.positionCount - 1, ray.origin + ray.direction * remainingLenght);
+		}*/
+
+		//El timer del cooldown para el ataque
+		if(timer>= 0.0f && canCount)
+		{
+			timer -= Time.deltaTime;
+		}
+		else if (timer <= 0.0f && !canAttack)
+		{
+			canCount = false;
+			timer = 0.0f;
+			canAttack = true;
+		}
+
+		//Timer de la animacion, dejenlo ser, ese segundo extra queda mejor.
+		if (timerAnimation >= 0.0f && canCount2)
+		{
+			timerAnimation -= Time.deltaTime;
+		}
+		else if (timerAnimation <= 0.0f && canAnimateAttack)
+		{
+			canCount2 = false;
+			timerAnimation = 0.0f;
+			canAnimateAttack = false;
+			animator.SetFloat("OnAttack", 0);
+		}
 	}
 	public void FixedUpdate()
 	{
-		rb.MovePosition((Vector2)rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
+		if(canMove)
+        {
+			rb.MovePosition((Vector2)rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
+		}
+		
 
 		//Para obtener la direccion a cual mirar segun donde este el mouse. NO BORRAR
 		/*Vector2 lookDir = (Vector2)actualPositionMouse - (Vector2)rb.position;
@@ -144,7 +188,10 @@ public class PlayerBehaviour : MonoBehaviour
 		health -= enemyDamage;
 		if (health <= 0)
 		{
-			PlayerDie();
+            if (canDie)
+            {
+				PlayerDie();
+			}
 		}
 	}
 	void PlayerDie()
@@ -181,6 +228,14 @@ public class PlayerBehaviour : MonoBehaviour
 				//Acá pasaria algo quizas? Un cartel que diga que falta la llave, o bueno, nada.
 				Debug.Log("No tenes la llave");
 			}
+		}
+
+		//Colleciona monedas
+		if (col.gameObject.CompareTag("collectable"))
+		{
+			coins += 1;
+			var coin = col.gameObject;
+			Destroy(coin);
 		}
 	}
 }
