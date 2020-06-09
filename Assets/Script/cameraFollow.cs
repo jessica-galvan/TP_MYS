3bc812c3fafd924d4d49e86406186bb369bbb59a
Franco Rivas <francorivaa19@gmail.com>
2020-06-09 02:56:18 -0300

    Música loopeada en menú inicio y Camara
    eso y ahora la camara ya no es la de cinemachine, tiene un script hecho para seguir al jugador

---
+++ Assets/Script/cameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = player.position.x;
+        temp.y = player.position.y;
+        transform.position = temp;
+    }
+}
